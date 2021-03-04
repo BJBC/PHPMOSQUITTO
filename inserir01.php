6ab57d68586549172072be18b97da61bb8415820
BJBC <bcaldasbc@gmail.com>
2021-03-04 20:25:33 -0300

    seg

---
+++ inserir01.php
@@ -0,0 +1,44 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "eventos";
+$table = "leitura";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error){;
+	echo "Erro de conexao: " . $conn->connect_error;
+}else{
+	for ($i=0; $i<22;$i++){
+		$stmt = $conn->prepare("INSERT INTO $table (topico,id_sensor,valor) VALUES(?,?,?)");
+    	$stmt->bind_param("sss", $topi, $idsen, $vall);
+    	$topi = "1/2/3";
+    	$idsen = "4";//temperatura
+    	$vall = rand(0,40);
+    	$stmt->execute();
+    	
+    	$idsen = "5";
+    	$vall = rand(50,100);//umidade
+    	$stmt->execute();
+
+    	$idsen = "2";
+    	$vall = "1";//falta de fase
+    	$stmt->execute();
+
+    	$idsen = "1";
+    	$vall = "1";//presença
+    	$stmt->execute();
+
+    	//if($stmt->execute()===TRUE){
+    	//	echo "salvei: " . $i ."<br>";
+    	//}
+
+    	sleep(5);
+	}
+	$stmt->close();
+	echo "Registros salvos" ."<br>";
+
+}
+?>
--- pdo01.php
+++ pdo01.php
@@ -3,6 +3,22 @@
 //CRIAR IMAGEM DO SERVIDOR AWS
 //https://www.youtube.com/watch?v=yREIVJWRr7o
 
+//criar grafo simples com php
+//https://www.devmedia.com.br/criando-graficos-com-php/11466
+
+//usando banco de dados 
+//https://www.gigasystems.com.br/artigo/16/como-exibir-graficos-em-php
+
+
+//BIBLIOTECA PHP PLOT
+
+//FAZENDO GRAFICO COM PHP E MYSQL
+//SELECT valor FROM leitura WHERE id_sensor = 1 ORDER BY datahora DESC LIMIT 5
+
+
+ 
+https://www.youtube.com/watch?v=Z62QAN5mP5M
+
 //USANDO PDO	
 $servername = "localhost"; $username = "root";
 $password = ""; $sgbdname = "eventos"; $table = "empresa";
