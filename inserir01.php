bb234b3245f34428133bd8a9b48ac0bb81360ec3
BJBC <bcaldasbc@gmail.com>
2021-04-01 21:18:54 -0300

    Update inserir01.php

--- inserir01.php
+++ inserir01.php
@@ -8,12 +8,13 @@ $table = "leitura";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error){;
+if ($conn->connect_error){
 	echo "Erro de conexao: " . $conn->connect_error;
 }else{
 	for ($i=0; $i<22;$i++){
 		$stmt = $conn->prepare("INSERT INTO $table (topico,id_sensor,valor) VALUES(?,?,?)");
     	$stmt->bind_param("sss", $topi, $idsen, $vall);
+    	//empresa/unidade/placa CLP
     	$topi = "1/2/3";
     	$idsen = "4";//temperatura
     	$vall = rand(0,40);
